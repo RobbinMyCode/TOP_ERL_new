Bugfix masking - n_step_return_vf masks exact (--> segment starting at index 99 can not generate an action) - q_loss overlaps (minimum 1 action generated, but not exceeding index 99 [except for segment init state=99]
speedup: torch parallization of slow forloop in segments_n_step_return_vf

mprl/rl/agent/top_erl_agent.py
@@ -479,20 +479,57 @@ class TopErlAgent(AbstractAgent):
             # [num_traj, traj_length] -> [num_traj, num_segments, num_seg_actions]
             future_returns[..., 1:] = future_v_pad_zero_end[:, v_idx]
         else:
-            #assign future_v in correct positions of future_returns [num_traj, total_steps] -> [num_traj, actual_num_segments, max_poss_len_segments]
-            for j in range(future_returns.shape[1]):
-                start = dataset["split_start_indexes"][:, j]
-                end = dataset["split_start_indexes"][:, j + 1] if j != actions.shape[1] - 1 else \
-                future_v.shape[-1] * torch.ones_like(start)
+            '''
+            def resample_n_step():
+                #do for rewards and returns
+                rewards_reshaped = torch.zeros_like(future_returns)
+                #assign future_v in correct positions of future_returns [num_traj, total_steps] -> [num_traj, actual_num_segments, max_poss_len_segments]
+                for j in range(future_returns.shape[1]):
+                    start = dataset["split_start_indexes"][:, j]
+                    end = dataset["split_start_indexes"][:, j + 1] if j != actions.shape[1] - 1 else \
+                    future_v.shape[-1] * torch.ones_like(start)
 
-                for b in range(future_returns.shape[0]):
-                    s = start[b]
-                    e = end[b]
-                    if e-s!=0: #edgecase e.g. both=0 -> [..., 1:0] = [..., 0:-1] would be incorrect
-                        future_returns[b, j, 1:e - s] = future_v_pad_zero_end[b, s:e-1]
+                    for b in range(future_returns.shape[0]):
+                        s = start[b]
+                        e = end[b]
+                        if e-s!=0: #edgecase e.g. both=0 -> [..., 1:0] = [..., 0:-1] would be incorrect
+                            future_returns[b, j, 1:e - s] = future_v_pad_zero_end[b, s:e-1]
+                            rewards_reshaped[b, j, :e - s] = rewards[b, s:e]
+                return future_returns, rewards_reshaped
 
+            future_returns1, rewards_reshaped1 = resample_n_step()
+            '''
+            start = dataset["split_start_indexes"]
 
-        ########################################################################
+            last_idx_append = future_v.shape[-1]* torch.ones((*start.shape[:-1], 1), dtype=torch.int, device=self.device)
+            end = torch.cat((dataset["split_start_indexes"][:, 1:], last_idx_append), dim=-1)
+
+            length = end - start
+
+            #create a mask for indexes where values shall be applied
+            pos = torch.arange(future_returns.shape[-1], device=self.device)[None, None, :]
+            mask = pos < length.unsqueeze(-1)
+
+            #reward sorted by splits
+            indices = torch.clamp(idx_in_segments, min=0, max=future_v.shape[-1]-1)
+
+            rewards_reshaped = torch.where(
+                mask,
+                torch.gather(rewards[:, None, :].expand(-1, dataset["split_start_indexes"].shape[1], -1), 2, indices),
+                torch.zeros_like(indices, dtype=rewards.dtype)
+            )
+
+            #returns sorted by splits
+            indices_v = torch.clamp(indices - 1, min=0)
+            g_v = torch.gather(
+                future_v_pad_zero_end.unsqueeze(1).expand(-1, dataset["split_start_indexes"].shape[1], -1),  # [B,J,T_v]
+                2,
+                indices_v
+            )
+            write_mask = (pos > 0) & mask
+            future_returns[write_mask] = g_v.to(torch.float)[write_mask]
+
+            ########################################################################
         ######### Compute rewards in the future, i.e. Eq(8) RHS 1st term #######
         ########################################################################
 
@@ -507,26 +544,31 @@ class TopErlAgent(AbstractAgent):
         # [num_traj, num_segments, 1 + num_seg_actions]
         discount_return = future_returns * discount_seq[:, None, :]
 
-        # [num_traj, traj_length] -> [num_traj, traj_length + num_seg_actions]
-        future_r_pad_zero_end \
-            = torch.nn.functional.pad(rewards, (0, num_seg_actions))
 
-        # -> [num_traj, num_segments, traj_length + num_seg_actions]
-        future_r_pad_zero_end = util.add_expand_dim(future_r_pad_zero_end,
-                                                    [1], [num_segments])
 
         # -> [num_traj, num_segments, 1 + num_seg_actions]
         if not "rand" in self.reference_split_args["split_strategy"]:
+            # [num_traj, traj_length] -> [num_traj, traj_length + num_seg_actions]
+            future_r_pad_zero_end \
+                = torch.nn.functional.pad(rewards, (0, num_seg_actions))
+
+            # -> [num_traj, num_segments, traj_length + num_seg_actions]
+            future_r_pad_zero_end = util.add_expand_dim(future_r_pad_zero_end,
+                                                        [1], [num_segments])
             seg_reward_idx = util.add_expand_dim(idx_in_segments, [0],
                                              [num_traj])
+
+            # torch.gather shapes
+            # input: [num_traj, num_segments, traj_length + num_seg_actions]
+            # index: [num_traj, num_segments, 1 + num_seg_actions]
+            # result: [num_traj, num_segments, 1 + num_seg_actions]
+            seg_r = torch.gather(input=future_r_pad_zero_end, dim=-1,
+                                 index=seg_reward_idx)
+
         else:
-            seg_reward_idx = idx_in_segments
-        # torch.gather shapes
-        # input: [num_traj, num_segments, traj_length + num_seg_actions]
-        # index: [num_traj, num_segments, 1 + num_seg_actions]
-        # result: [num_traj, num_segments, 1 + num_seg_actions]
-        seg_r = torch.gather(input=future_r_pad_zero_end, dim=-1,
-                             index=seg_reward_idx)
+            #valid in splits -> use reshaped one
+            seg_r = rewards_reshaped
+
 
         seg_discount_r = seg_r * discount_seq[:, None, :]
 
@@ -553,6 +595,22 @@ class TopErlAgent(AbstractAgent):
 
         # [num_traj, num_segments, 1 + num_seg_actions]
         n_step_returns = (tril_discount_rewards.sum(dim=-1) + discount_return)
+
+        #filter out invalid commulativ returns (for steps not actually done in this split)
+        if "rand" in self.reference_split_args["split_strategy"]:
+            next_seg_start_idx = seg_start_idx[..., 1:]
+            valid_q_idx_split = idx_in_segments[:, :-1, :] < next_seg_start_idx[..., None]
+            # last split will never collide with the next split_start -> add True here
+            valid_q_idx_split_full = torch.cat([valid_q_idx_split, torch.ones(
+                (*(valid_q_idx_split.shape[:-2]), 1, valid_q_idx_split.shape[-1]), dtype=torch.bool,
+                device=self.device)], dim=-2)
+
+            valid_q_idx_max_len = idx_in_segments[..., :] < self.traj_length
+            # only take in actions, first of valid_q_idx_split is the start state of each segment -> and statement with absolute length
+            valid_mask = valid_q_idx_split_full * valid_q_idx_max_len
+
+            n_step_returns = n_step_returns * valid_mask
+
         return n_step_returns
 
     def update_critic(self):
@@ -643,7 +701,8 @@ class TopErlAgent(AbstractAgent):
                         valid_q_idx_split = idx_in_segments[:, :-1, :] < next_seg_start_idx[..., None]
                         valid_q_idx_split_full = torch.cat([valid_q_idx_split, torch.ones((*(valid_q_idx_split.shape[:-2]), 1, valid_q_idx_split.shape[-1]), dtype=torch.bool, device=self.device)], dim=-2)
 
-                        valid_q_idx_max_len = seg_actions_idx < self.traj_length
+                        #we only count actions -> one step [=init pos] less
+                        valid_q_idx_max_len = seg_actions_idx < self.traj_length - 1
                         #only take in actions, first of valid_q_idx_split is the start state of each segment -> and statement with absolute length
                         valid_mask = valid_q_idx_split_full[..., 1:] * valid_q_idx_max_len
                     # [num_traj, num_segments, num_seg_actions]
@@ -820,10 +879,15 @@ class TopErlAgent(AbstractAgent):
 
         #mask out values from "invalid" actions from q -> set to 0 (only required for the "oversampling" in random_size_range)
         if "rand" in self.reference_split_args["split_strategy"]:
+            #last segment ends at index 99 as we cut out the init states, e.g. else 19 actions for splitlength 19 (=state+18 ations)
+            #problem: last start at 99 -> no action generated ==> exception to the rule
+            #not required for prior states as the last action leads to the initial state of the next segment -> still valid
+            #also all segment lengths != 0 should sample an action
             seg_end_idx = torch.cat([seg_start_idx[:, 1:],
-                                     states.size(1) * torch.ones((seg_start_idx.size(0), 1), dtype=torch.int,
+                                     (states.size(1)-1) * torch.ones((seg_start_idx.size(0) , 1), dtype=torch.int,
                                                                  device=self.device)], axis=-1)
             seg_len_idx = seg_end_idx - seg_start_idx
+            seg_len_idx[seg_start_idx == states.size(1)-1] = 1
             index_list = torch.arange(q2.size(-1), device=self.device)
             mask = index_list[None, None, :] < seg_len_idx[:, :, None]
             q2 = q2 * mask

mprl/util/util_plotting.py
@@ -39,13 +39,19 @@ def plot_index_histogram(split_strategy: dict={'correction_completion': 'current
 
         total_hits[start_hits] += 1
 
-    total_hits[0] = 0 #first split always starts at t=0 -> not interesting
+    # first split always starts at t=0 -> not interesting, also additional zero splits at end not interesting
+    total_hits[-1] = 0
+    total_hits[0] = 0
     #plt.xticks(np.arange(len(total_hits)))
     if split_strategy["split_strategy"] == "random_size_range":
         plt.title(split_strategy["split_strategy"] +f" range: {split_strategy['size_range']} n_split={split_strategy['n_splits']}")
     elif split_strategy["split_strategy"] == "fixed_size_rand_start":
         plt.title(split_strategy[
                       "split_strategy"] + f" fixed_size: {split_strategy['fixed_size']}")
+    elif split_strategy["split_strategy"] == "rand_semi_fixed_size_focus_on_region":
+        plt.title(f" fixed_size: {split_strategy['fixed_size']}, fr={split_strategy['focus_regions']},"
+                                          f" fr_region_size={split_strategy['focus_regions_size_1way']}), "
+                                          f"fr_sample_min={split_strategy['min_decreased_size']}, ")
     plt.bar(np.arange(len(total_hits)), total_hits)
     plt.show()
 
@@ -63,7 +69,21 @@ for i in range(6,10):
 split_strategy["split_strategy"] = "fixed_size_rand_start"
 split_strategy["fixed_size"] = 20
 
-sizes = [5,10,15,20,25,30]
-for i in range(len(sizes)):
-    split_strategy["fixed_size"] = sizes[i]
-    plot_index_histogram(split_strategy)
+
+
+#sizes = [5,10,15,20,25,30]
+#for i in range(len(sizes)):
+#    split_strategy["fixed_size"] = sizes[i]
+#    plot_index_histogram(split_strategy)
+
+split_strategy["fixed_size"] = 20
+split_strategy["split_strategy"] = "rand_semi_fixed_size_focus_on_region"
+split_strategy["focus_regions"]= [50]
+split_strategy["focus_regions_size_1way"] = 25
+split_strategy["min_decreased_size"] = 10
+split_strategy["n_splits"] = 15
+sizes = [10,15,20,25,30,35,40,45,50]
+#for i in range(len(sizes)):
+    #split_strategy["min_decreased_size"] = sizes[i]
+    #split_strategy["focus_regions_size_1way"] = sizes[i]
+plot_index_histogram(split_strategy)

mprl/util/util_sampling_split.py
@@ -91,16 +91,69 @@ def get_splits(times, split_strategy: dict={"split_strategy": "n_equal_splits", 
         else: #-> did not reahc end in n_splits-1 steps --> put rest in nth bucket
             split_size_list.append(times.size(-1) - total_size_covered)
     elif split_strategy["split_strategy"] == "fixed_size_rand_start":
-        #randint between 1 and fixed_size -1
-        split_size_list = [np.random.randint(0, split_strategy["fixed_size"])]
+        #randint between 1 and fixed_size [upper limit not include in randint; low <= x < high]
+        split_size_list = [np.random.randint(1, split_strategy["fixed_size"]+1)]
         total_size_covered = split_size_list[0]
 
         split_size_list = split_size_list + [split_strategy["fixed_size"]] * ((times.size(-1) -total_size_covered) // split_strategy["fixed_size"])
-        total_size_covered += split_strategy["fixed_size"] * ((times.size(-1) -total_size_covered) // split_strategy["fixed_size"])
+        total_size_covered = total_size_covered + split_strategy["fixed_size"] * ((times.size(-1) -total_size_covered) // split_strategy["fixed_size"])
         if times.size(-1) != total_size_covered:
             split_size_list.append(times.size(-1) - total_size_covered)
         if len(split_size_list) < split_strategy["n_splits"]:
             split_size_list = split_size_list + [0] * (split_strategy["n_splits"] - len(split_size_list))
+        '''
+            focus_regions: [30, 80] #each value gives a seperate region 
+                                #[focus_region[i]-focus_regions_size_1way, focus_region[i]+focus_regions_size_1way], 
+                                #around where the sampling size is decreased 
+                                #-- uniformly sampled in [min_decreased_size, fixed_size] (include_last=True) 
+        focus_regions_size_1way: 20 #(half-)size of focus region
+        min_decreased_size: 10  #min sampling distance in focus region
+        '''
+    elif split_strategy["split_strategy"] == "rand_semi_fixed_size_focus_on_region":
+        focus_regions_start = np.array(split_strategy["focus_regions"]) - split_strategy["focus_regions_size_1way"]
+        focus_regions_end = np.array(split_strategy["focus_regions"]) + split_strategy["focus_regions_size_1way"]
+        fr_min = split_strategy["min_decreased_size"]
+
+        split_size_list = [np.random.randint(0, split_strategy["fixed_size"])]
+        total_size_covered = split_size_list[0]
+        fr_idx = 0
+        fr_reached = False
+        next_potential_pos = total_size_covered + split_strategy["fixed_size"]
+        while total_size_covered < times.size(-1):
+            #make sure last segment is not too large [ignores focus_regions]
+            if times.size(-1) -total_size_covered < split_strategy["fixed_size"]:
+                split_size_list.append(times.size(-1) - total_size_covered)
+                total_size_covered = times.size(-1)
+            #ensure focus regions are sampled accordingly
+            elif next_potential_pos > focus_regions_start[fr_idx] and total_size_covered < focus_regions_end[fr_idx]:
+                fr_reached = True
+                next_split = np.random.randint(fr_min, split_strategy["fixed_size"])
+                split_size_list = split_size_list + [next_split]
+                total_size_covered += next_split
+            else:
+                if fr_reached:
+                    if fr_idx != len(focus_regions_start) - 1:
+                        fr_idx += 1
+                    fr_reached = False
+                #get one more iteration before adding a fixed_size in case the next focus_region is already reached
+                # --> "pass" and only in the next call we add the default size
+                else:
+                    jitter = 0 #int(np.random.normal(0, 2))
+                    if total_size_covered + jitter + split_strategy["fixed_size"] > times.size(-1):
+                        jitter = 0
+                    split_size_list = split_size_list + [split_strategy["fixed_size"]+ jitter]
+                    total_size_covered += split_strategy["fixed_size"] + jitter
+
+
+        if len(split_size_list) < split_strategy["n_splits"]:
+            split_size_list = split_size_list + [0] * (split_strategy["n_splits"] - len(split_size_list))
+
+        assert len(split_size_list) <= split_strategy["n_splits"], (f""
+                f"'n_splits'={split_strategy['n_splits']} is not sufficient for the given split_strategy "
+                f"'rand_semi_fixed_size_focus_on_region' with fixed_size={split_strategy['fixed_size']} and "
+                f"min_decreased_size={split_strategy['min_decreased_size']}. It requires {len(split_size_list)} or more splits.")
+
+
 
 
 